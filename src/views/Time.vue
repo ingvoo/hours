a0544bc621d9d71478d58b2ec818b6296adf8546
ingvijonasson <ingvi101@gmail.com>
2021-11-19 07:53:32 +0100

    chore: add basic styles to time tracker

--- src/views/Time.vue
+++ src/views/Time.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="container mx-auto">
-    <div class="flex justify-between w-full">
-      <h3>Project Name</h3>
-      <h4>Task</h4>
+    <h2 class="text-xl font-bold">19. November 2021</h2>
+    <div class="flex justify-between border-t mt-4 py-2">
+      <div>
+        <h3 class="text-xl font-bold">Ljomi</h3>
+        <h4 class="text-lg font-bold">Learning Vue 3 with TS</h4>
+      </div>
+      <div class="flex items-center">
+        <span>{{ counter }} Minutes </span>
+        <button class="ml-2 border p-2" @click="timerStart">Start</button>
+        <button class="ml-2 border p-2">Edit</button>
+      </div>
     </div>
-    <span>0</span>
-    <button>Start</button>
-    <button>Edit</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      counter: 120,
+    }
+  },
+  methods: {
+    timerStart() {
+      this.counter++
+    },
+  },
+}
+</script>
