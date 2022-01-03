f5dce00b6820c1c13b6e20d15e54431ec76b0dbc
ingvijonasson <ingvi101@gmail.com>
2022-01-03 22:58:47 +0100

    refactor: remove build in json-server and setup endpoints from heroku app

--- src/services/HoursService.ts
+++ src/services/HoursService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://ingvoo-hours-api.herokuapp.com/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
